b32321a163db7c75c4c8a479ebea0a3ab7493353
Yelson Chevarrias <chevarrias@gmail.com>
2019-11-07 21:25:08 -0500

    FormsModule (parte 2)

--- Angular/Clase04/appFormulario/src/app/registro/registro.component.ts
+++ Angular/Clase04/appFormulario/src/app/registro/registro.component.ts
@@ -19,10 +19,23 @@ export class RegistroComponent implements OnInit {
 	registrar() {
 		if (this.formularioRegistro.valid) {
 			console.log("Información ha sido grabada")
+			console.log(this.formularioRegistro.value)
+			this.formularioRegistro.reset({ nombre: "Zoila" })
 		} else {
 			console.log("El formulario no es válido")
 		}
 		// console.log(this.formularioRegistro)
 	}
+
+	cargarTodo() {
+		this.formularioRegistro.setValue({ nombre: "Marcela", correo: "marcela@correo.com", contrasena: "1234" })
+	}
+
+	cargarParcial() {
+		this.formularioRegistro.form.patchValue({ correo: "marcela@correo.com", contrasena: "1234" })
+	}
+
+
+
 
 }
